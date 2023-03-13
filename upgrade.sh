a5f86b574264493e55e7737fa1a4eb5529249fa4
Tyler Jang <tyler@trunk.io>
2023-03-13 15:18:29 -0700

    symlink trunk path if it doesnt exist for git hook

--- upgrade.sh
+++ upgrade.sh
@@ -5,11 +5,16 @@ set -euo pipefail
 echo "RUNNING THE COMMAND <${TRUNK_PATH} upgrade --no-progress -n ${UPGRADE_ARGUMENTS}>"
 # trunk-ignore(shellcheck/SC2086): pass arguments directly as is
 upgrade_output=$(${TRUNK_PATH} upgrade --no-progress -n ${UPGRADE_ARGUMENTS})
+# TODO: TYLER TRIM THE OUTPUT FOR AN INSTALL
 new_cli_version=$(echo "${upgrade_output}" | grep "cli upgrade" | awk '{print $NF}' | sed -e 's/\x1b\[[0-9;]*m//g')
 title_message="Upgrade trunk"
 
 if [[ -n ${new_cli_version} ]]; then
   title_message="Upgrade trunk to ${new_cli_version}"
+fi
+
+if [ ! -e trunk ]; then
+  ln -s "${TRUNK_PATH}" trunk
 fi
 
 echo "Finished running upgrade"
