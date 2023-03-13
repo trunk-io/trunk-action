fbedd7d49d341b50e169cb7aad646f8c3875db4b
Tyler Jang <tyler@trunk.io>
2023-03-13 14:17:03 -0700

    add upgrade option

---
+++ upgrade.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+set -euo pipefail
+
+upgrade_output=$(${TRUNK_PATH} upgrade -n ${UPGRADE_ARGUMENTS})
+new_cli_version=$(echo ${upgrade_output} | grep "cli upgrade" | awk '{print $NF}' | sed -e 's/\x1b\[[0-9;]*m//g')
+title_message="Upgrade trunk"
+
+if [[ -n new_cli_version ]]; then
+  title_message="Upgrade trunk to ${new_cli_version}"
+fi
+
+echo "UPGRADE_OUTPUT=${upgrade_output}" >>$GITHUB_OUTPUT
+echo "TITLE_MESSAGE=${title_message}" >>$GITHUB_OUTPUT
