6fbe6f281ed0f1c7eef13d1fed2021fbabafc9a9
Sam Lijin <sam@trunk.io>
2023-03-28 14:09:25 -0700

    feat: introduce check-all-mode
    Enables users of `check --all` to compare against the previous upload.

--- all.sh
+++ all.sh
@@ -11,6 +11,25 @@ if [[ -z ${INPUT_TRUNK_TOKEN} ]]; then
     --output-file .trunk/landing-state.json \
     --github-commit "${GITHUB_SHA}" \
     ${INPUT_ARGUMENTS}
+elif [[ ${CHECK_ALL_MODE} == "hold-the-line" ]]; then
+  latest_raw_upload="$(mktemp)"
+  prev_ref=$("${TRUNK_PATH}" get-latest-raw-upload \
+    --series "${INPUT_UPLOAD_SERIES:-${GITHUB_REF_NAME}}" \
+    --token "${INPUT_TRUNK_TOKEN}" \
+    "${latest_raw_upload}") || prev_ref="NOT_FOUND"
+  if [[ ${prev_ref} == "NOT_FOUND" ]]; then
+    htl_arg=""
+  else
+    htl_arg="--htl-factories-path=${latest_raw_upload}"
+    git fetch "${prev_ref}"
+  fi
+  "${TRUNK_PATH}" check \
+    --all \
+    --upload \
+    ${htl_arg} \
+    --series "${INPUT_UPLOAD_SERIES:-${GITHUB_REF_NAME}}" \
+    --token "${INPUT_TRUNK_TOKEN}" \
+    ${INPUT_ARGUMENTS}
 else
   "${TRUNK_PATH}" check \
     --all \
