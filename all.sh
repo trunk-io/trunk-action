ed5355a82540a7b8128d367b931c937b80d66773
Samuel Lijin <sam@trunk.io>
2023-04-19 19:05:38 -0700

    Update all.sh

--- all.sh
+++ all.sh
@@ -16,7 +16,7 @@ if [[ -z ${INPUT_TRUNK_TOKEN} ]]; then
     --github-commit "${GITHUB_SHA}" \
     ${INPUT_ARGUMENTS}
 elif [[ ${INPUT_CHECK_ALL_MODE} == "hold-the-line" ]]; then
-  prev_ref="$("${TRUNK_PATH}" get-latest-raw-upload \
+  prev_ref="$("${TRUNK_PATH}" check get-latest-raw-output \
     --series "${INPUT_UPLOAD_SERIES:-${GITHUB_REF_NAME}}" \
     --token "${INPUT_TRUNK_TOKEN}" \
     "${latest_raw_upload}")"
