15e9ed082942e41f24c41aa93ad6df0c9b81e7fc
Samuel Lijin <sam@trunk.io>
2023-04-20 22:54:13 -0700

    chore(check): special-case the "first upload to series" scenario (#82)
    See trunk-io/trunk#8263

--- all.sh
+++ all.sh
@@ -15,25 +15,16 @@ if [[ -z ${INPUT_TRUNK_TOKEN} ]]; then
     --github-commit "${GITHUB_SHA}" \
     ${INPUT_ARGUMENTS}
 elif [[ ${INPUT_CHECK_ALL_MODE} == "hold-the-line" ]]; then
-  latest_raw_upload="$(mktemp)"
-  # We have to tolerate failures here: the first attempt to download the latest
-  # upload for a given repo/series will always fail, because none has yet been
-  # uploaded. However, we also don't want to silently suppress errors if they
-  # occur: the user should be able to see them without having to set INPUT_DEBUG.
-  set +e
-  prev_ref="$("${TRUNK_PATH}" get-latest-raw-upload \
+  prev_ref="$("${TRUNK_PATH}" check get-latest-raw-output \
     --series "${INPUT_UPLOAD_SERIES:-${GITHUB_REF_NAME}}" \
     --token "${INPUT_TRUNK_TOKEN}" \
     "${latest_raw_upload}")"
-  get_latest_raw_upload_exit_code=$?
-  set -e
-  if [[ ${get_latest_raw_upload_exit_code} == 0 ]]; then
+  if [[ ${prev_ref} =~ .*UNSPECIFIED.* ]]; then
+    echo "${prev_ref}"
+    htl_arg=""
+  else
     htl_arg="--htl-factories-path=${latest_raw_upload}"
     git fetch origin "${prev_ref}"
-  else
-    echo "Failed to retrieve the latest upload. This is normal if no uploads"
-    echo "have been made to this series before."
-    htl_arg=""
   fi
   "${TRUNK_PATH}" check \
     --all \
