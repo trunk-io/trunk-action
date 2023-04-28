7b254457887fe837ce81ad6ab9237b2147d02bab
Maverick Chung <maverick@trunk.io>
2023-04-28 13:55:59 -0700

    added version checking

--- all.sh
+++ all.sh
@@ -8,6 +8,19 @@ if [[ ${INPUT_DEBUG} == "true" ]]; then
   set -x
 fi
 
+TRUNK_VERSION=$("${TRUNK_PATH}" --version)
+MINIMUM_CI_VERSION="1.8.2-beta.12"
+
+echo "Trunk version: ${TRUNK_VERSION}"
+
+# trunk-ignore-begin(shellcheck/SC2312): the == will fail if anything inside the $() fails
+if [[ "$(printf "%s\n%s\n" "${MINIMUM_CI_VERSION}" "${TRUNK_VERSION}" |
+  sort --version-sort |
+  head -n 1)" == "${MINIMUM_CI_VERSION}"* ]] || [[ ${TRUNK_VERSION} == "0.0.0" ]]; then
+  CI_ARGUMENT="--ci "
+fi
+# trunk-ignore-end(shellcheck/SC2312)
+
 if [[ -z ${INPUT_TRUNK_TOKEN} ]]; then
   "${TRUNK_PATH}" check \
     --ci \
@@ -28,7 +41,7 @@ elif [[ ${INPUT_CHECK_ALL_MODE} == "hold-the-line" ]]; then
     git fetch origin "${prev_ref}"
   fi
   "${TRUNK_PATH}" check \
-    --ci \
+    ${CI_ARGUMENT} \
     --all \
     --upload \
     ${htl_arg} \
@@ -37,7 +50,7 @@ elif [[ ${INPUT_CHECK_ALL_MODE} == "hold-the-line" ]]; then
     ${INPUT_ARGUMENTS}
 else
   "${TRUNK_PATH}" check \
-    --ci \
+    ${CI_ARGUMENT} \
     --all \
     --upload \
     --series "${INPUT_UPLOAD_SERIES:-${INPUT_GITHUB_REF_NAME}}" \
