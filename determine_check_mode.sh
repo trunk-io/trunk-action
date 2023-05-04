4cf8ffc93fdf2272d37e72e707b7e21aab74263d
Tyler Jang <tyler@trunk.io>
2023-05-04 10:25:44 -0700

    fix for push as well

--- determine_check_mode.sh
+++ determine_check_mode.sh
@@ -10,7 +10,7 @@ check_mode="${INPUT_CHECK_MODE}"
 if [[ -z ${check_mode} ]]; then
   if [[ ${GITHUB_EVENT_NAME} == "pull_request" || ${GITHUB_EVENT_NAME} == "pull_request_target" ]]; then
     check_mode="pull_request"
-  elif [[ ${GITHUB_EVENT_NAME} == "push" && ${GITHUB_REF_NAME} == trunk-merge/* ]]; then
+  elif [[ ${GITHUB_EVENT_NAME} == "push" && (${GITHUB_REF_NAME} == trunk-merge/* || ${GITHUB_REF_NAME} == trunk-merge-beta/*) ]]; then
     check_mode="trunk_merge"
   elif [[ ${GITHUB_EVENT_NAME} == "push" ]]; then
     check_mode="push"
