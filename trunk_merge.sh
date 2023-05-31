e6a608f09aed4235e5698c450c6cee646ec72497
jenny <jennygzma@gmail.com>
2023-05-31 13:46:34 -0400

    duplicate trunk check script in trunk_merge.sh

--- trunk_merge.sh
+++ trunk_merge.sh
@@ -26,3 +26,22 @@ echo "Detected merge queue commit, using HEAD^1 (${upstream}) as upstream and HE
   --upstream "${upstream}" \
   --github-commit "${git_commit}" \
   ${INPUT_ARGUMENTS}
+
+if [[ -n ${INPUT_TRUNK_TOKEN} ]]; then
+  "${TRUNK_PATH}" check \
+    --ci \
+    --upstream "${upstream}" \
+    --github-commit "${git_commit}" \
+    --github-label "${INPUT_LABEL}" \
+    --token "${INPUT_TRUNK_TOKEN}" \
+    "${annotation_argument}" \
+    ${INPUT_ARGUMENTS}
+else
+  "${TRUNK_PATH}" check \
+    --ci \
+    --upstream "${upstream}" \
+    --github-commit "${git_commit}" \
+    --github-label "${INPUT_LABEL}" \
+    "${annotation_argument}" \
+    ${INPUT_ARGUMENTS}
+fi
