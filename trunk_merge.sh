8dbb566997eef213a789a709c14504eb3b89e55a
jenny <jennygzma@gmail.com>
2023-05-31 13:48:18 -0400

    Update trunk_merge.sh

--- trunk_merge.sh
+++ trunk_merge.sh
@@ -21,12 +21,6 @@ upstream=$(git rev-parse HEAD^1)
 git_commit=$(git rev-parse HEAD^2)
 echo "Detected merge queue commit, using HEAD^1 (${upstream}) as upstream and HEAD^2 (${git_commit}) as github commit"
 
-"${TRUNK_PATH}" check \
-  --ci \
-  --upstream "${upstream}" \
-  --github-commit "${git_commit}" \
-  ${INPUT_ARGUMENTS}
-
 if [[ -n ${INPUT_TRUNK_TOKEN} ]]; then
   "${TRUNK_PATH}" check \
     --ci \
