1840284c465e628ebc34f8a3aa6c0dc35a0b084b
Maverick Chung <maverick@trunk.io>
2023-06-12 00:58:33 -0700

    of

--- mask-payload/mask.js
+++ mask-payload/mask.js
@@ -20,8 +20,8 @@ function hashFile(filename) {
 
 function envWrite({ payload, fd, varname, path, backup }) {
   toWrite = payload;
-  for (const arg in path.split(".")) {
-    console.log(JSON.stringify(arg), null, 2);
+  for (const arg of path.split(".")) {
+    console.log(JSON.stringify(arg, null, 2));
     toWrite = toWrite[arg] ?? {};
   }
   if (!["string", "boolean", "number"].some((type) => typeof toWrite === type)) {
@@ -49,50 +49,64 @@ function run() {
     fs.writeSync(githubEnv, Buffer.from(`TRUNK_TOKEN=${trunkToken}\n`));
 
     envVarConfigs = [
-      ["GITHUB_EVENT_PULL_REQUEST_BASE_REPO_OWNER", "pullRequest.base.repo.owner.login", ""],
-      ["GITHUB_EVENT_PULL_REQUEST_BASE_REPO_NAME", "pullRequest.base.repo.name", ""],
-      [
-        "GITHUB_EVENT_PULL_REQUEST_BASE_SHA",
-        "pullRequest.base.sha",
-        process.env.GITHUB_EVENT_PULL_REQUEST_BASE_SHA,
-      ],
-      [
-        "GITHUB_EVENT_PULL_REQUEST_HEAD_REPO_FORK",
-        "pullRequest.head.repo.fork",
-        process.env.GITHUB_EVENT_PULL_REQUEST_HEAD_REPO_FORK,
-      ],
-      [
-        "GITHUB_EVENT_PULL_REQUEST_HEAD_SHA",
-        "pullRequest.head.sha",
-        process.env.GITHUB_EVENT_PULL_REQUEST_HEAD_SHA,
-      ],
-      [
-        "GITHUB_EVENT_PULL_REQUEST_NUMBER",
-        "pullRequest.number",
-        process.env.GITHUB_EVENT_PULL_REQUEST_NUMBER,
-      ],
-      ["GITHUB_REF_NAME", "targetRefName", process.env.GITHUB_REF_NAME],
-      ["INPUT_ARGUMENTS", "arguments", getInput("arguments")],
-      ["INPUT_CACHE", "cache", getInput("cache")],
-      [
-        "INPUT_CACHE_KEY",
-        "cacheKey",
-        `trunk-${getInput("cache-key")}-${process.env.RUNNER_OS}-${hashFile(".trunk/trunk.yaml")}`,
-      ],
-      ["INPUT_CACHE_PATH", "cachePath", "~/.cache/trunk"],
-      ["INPUT_CHECK_ALL_MODE", "checkAllMode", getInput("check-all-mode")],
-      ["INPUT_CHECK_MODE", "checkMode", getInput("check-mode")],
-      ["INPUT_CHECK_RUN_ID", "checkRunId", getInput("check-run-id")],
-      ["INPUT_DEBUG", "debug", getInput("debug")],
-      ["INPUT_GITHUB_REF_NAME", "targetRefName", process.env.GITHUB_REF_NAME],
-      ["INPUT_LABEL", "label", getInput("label")],
-      ["INPUT_SETUP_CACHE_KEY", "setupCacheKey", getInput("cache-key")],
-      ["INPUT_SETUP_DEPS", "setupDeps", getInput("setup-deps")],
-      ["INPUT_TARGET_CHECKOUT", "targetCheckout", ""],
-      ["INPUT_TARGET_CHECKOUT_REF", "targetCheckoutRef", ""],
-      ["INPUT_TRUNK_PATH", "trunkPath", getInput("trunk-path")],
-      ["INPUT_UPLOAD_LANDING_STATE", "uploadLandingState", "false"],
-      ["INPUT_UPLOAD_SERIES", "uploadSeries", getInput("upload-series")],
+      {
+        varname: "GITHUB_EVENT_PULL_REQUEST_BASE_REPO_OWNER",
+        path: "pullRequest.base.repo.owner.login",
+        backup: "",
+      },
+      {
+        varname: "GITHUB_EVENT_PULL_REQUEST_BASE_REPO_NAME",
+        path: "pullRequest.base.repo.name",
+        backup: "",
+      },
+      {
+        varname: "GITHUB_EVENT_PULL_REQUEST_BASE_SHA",
+        path: "pullRequest.base.sha",
+        path: process.env.GITHUB_EVENT_PULL_REQUEST_BASE_SHA,
+      },
+      {
+        varname: "GITHUB_EVENT_PULL_REQUEST_HEAD_REPO_FORK",
+        path: "pullRequest.head.repo.fork",
+        path: process.env.GITHUB_EVENT_PULL_REQUEST_HEAD_REPO_FORK,
+      },
+      {
+        varname: "GITHUB_EVENT_PULL_REQUEST_HEAD_SHA",
+        path: "pullRequest.head.sha",
+        path: process.env.GITHUB_EVENT_PULL_REQUEST_HEAD_SHA,
+      },
+      {
+        varname: "GITHUB_EVENT_PULL_REQUEST_NUMBER",
+        path: "pullRequest.number",
+        path: process.env.GITHUB_EVENT_PULL_REQUEST_NUMBER,
+      },
+      { varname: "GITHUB_REF_NAME", path: "targetRefName", backup: process.env.GITHUB_REF_NAME },
+      { varname: "INPUT_ARGUMENTS", path: "arguments", backup: getInput("arguments") },
+      { varname: "INPUT_CACHE", path: "cache", backup: getInput("cache") },
+      {
+        varname: "INPUT_CACHE_KEY",
+        path: "cacheKey",
+        path: `trunk-${getInput("cache-key")}-${process.env.RUNNER_OS}-${hashFile(
+          ".trunk/trunk.yaml"
+        )}`,
+      },
+      { varname: "INPUT_CACHE_PATH", path: "cachePath", backup: "~/.cache/trunk" },
+      { varname: "INPUT_CHECK_ALL_MODE", path: "checkAllMode", backup: getInput("check-all-mode") },
+      { varname: "INPUT_CHECK_MODE", path: "checkMode", backup: getInput("check-mode") },
+      { varname: "INPUT_CHECK_RUN_ID", path: "checkRunId", backup: getInput("check-run-id") },
+      { varname: "INPUT_DEBUG", path: "debug", backup: getInput("debug") },
+      {
+        varname: "INPUT_GITHUB_REF_NAME",
+        path: "targetRefName",
+        backup: process.env.GITHUB_REF_NAME,
+      },
+      { varname: "INPUT_LABEL", path: "label", backup: getInput("label") },
+      { varname: "INPUT_SETUP_CACHE_KEY", path: "setupCacheKey", backup: getInput("cache-key") },
+      { varname: "INPUT_SETUP_DEPS", path: "setupDeps", backup: getInput("setup-deps") },
+      { varname: "INPUT_TARGET_CHECKOUT", path: "targetCheckout", backup: "" },
+      { varname: "INPUT_TARGET_CHECKOUT_REF", path: "targetCheckoutRef", backup: "" },
+      { varname: "INPUT_TRUNK_PATH", path: "trunkPath", backup: getInput("trunk-path") },
+      { varname: "INPUT_UPLOAD_LANDING_STATE", path: "uploadLandingState", backup: "false" },
+      { varname: "INPUT_UPLOAD_SERIES", path: "uploadSeries", backup: getInput("upload-series") },
     ];
 
     for (const [varname, path, backup] of envVarConfigs) {
