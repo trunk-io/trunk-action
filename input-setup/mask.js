459d49d75c84784a97de80867e762ceb740dabfd
Samuel Lijin <sam@trunk.io>
2023-06-14 14:59:35 -0700

    roll-forward: respect payload.trunkApiAddress (#133)
    Reverts trunk-io/trunk-action#131. Need trunk-io/trunk#9171 to be
    released first.
    
    ---------
    
    Co-authored-by: Maverick Chung <maverick@trunk.io>
    Co-authored-by: Maverick Chung <mavchung@gmail.com>

--- input-setup/mask.js
+++ input-setup/mask.js
@@ -40,110 +40,123 @@ function run() {
     const envVarConfigs = [
       {
         varname: "GITHUB_EVENT_PULL_REQUEST_BASE_REPO_OWNER",
-        payloadValue: payload?.pullRequest?.base?.repo?.owner?.login,
+        payloadValue: payload?.pullRequest?.base?.repo?.owner?.login ?? "",
         backup: "",
       },
       {
         varname: "GITHUB_EVENT_PULL_REQUEST_BASE_REPO_NAME",
-        payloadValue: payload?.pullRequest?.base?.repo?.name,
+        payloadValue: payload?.pullRequest?.base?.repo?.name ?? "",
         backup: "",
       },
       {
         varname: "GITHUB_EVENT_PULL_REQUEST_BASE_SHA",
-        payloadValue: payload?.pullRequest?.base?.sha,
+        payloadValue: payload?.pullRequest?.base?.sha ?? "",
         backup: githubEventPR?.base?.sha ?? "",
       },
       {
         varname: "GITHUB_EVENT_PULL_REQUEST_HEAD_REPO_FORK",
-        payloadValue: payload?.pullRequest?.head?.repo?.fork,
+        payloadValue: payload?.pullRequest?.head?.repo?.fork ?? "",
         backup: githubEventPR?.head?.repo?.fork ?? "",
       },
       {
         varname: "GITHUB_EVENT_PULL_REQUEST_HEAD_SHA",
-        payloadValue: payload?.pullRequest?.head?.sha,
+        payloadValue: payload?.pullRequest?.head?.sha ?? "",
         backup: githubEventPR?.head?.sha ?? "",
       },
       {
         varname: "GITHUB_EVENT_PULL_REQUEST_NUMBER",
-        payloadValue: payload?.pullRequest?.number,
+        payloadValue: payload?.pullRequest?.number ?? "",
         backup: githubEventPR?.number ?? "",
       },
       {
         varname: "GITHUB_REF_NAME",
-        payloadValue: payload?.targetRefName,
-        backup: process.env.GITHUB_REF_NAME,
+        payloadValue: payload?.targetRefName ?? "",
+        backup: process.env.GITHUB_REF_NAME ?? "",
       },
-      { varname: "INPUT_ARGUMENTS", payloadValue: payload?.arguments, backup: inputs.arguments },
-      { varname: "INPUT_CACHE", payloadValue: payload?.cache, backup: inputs.cache },
+      {
+        varname: "INPUT_ARGUMENTS",
+        payloadValue: payload?.arguments ?? "",
+        backup: inputs.arguments ?? "",
+      },
+      { varname: "INPUT_CACHE", payloadValue: payload?.cache ?? "", backup: inputs.cache ?? "" },
       {
         varname: "INPUT_CACHE_KEY",
-        payloadValue: payload?.cacheKey,
+        payloadValue: payload?.cacheKey ?? "",
         backup: `trunk-${inputs["cache-key"]}-${process.env.RUNNER_OS}-${hashFile(
           ".trunk/trunk.yaml"
         )}`,
       },
-      { varname: "INPUT_CACHE_PATH", payloadValue: payload?.cachePath, backup: "~/.cache/trunk" },
+      {
+        varname: "INPUT_CACHE_PATH",
+        payloadValue: payload?.cachePath ?? "",
+        backup: "~/.cache/trunk",
+      },
       {
         varname: "INPUT_CHECK_ALL_MODE",
-        payloadValue: payload?.checkAllMode,
-        backup: inputs["check-all-mode"],
+        payloadValue: payload?.checkAllMode ?? "",
+        backup: inputs["check-all-mode"] ?? "",
       },
       {
         varname: "INPUT_CHECK_MODE",
-        payloadValue: payload?.checkMode,
-        backup: inputs["check-mode"],
+        payloadValue: payload?.checkMode ?? "",
+        backup: inputs["check-mode"] ?? "",
       },
       {
         varname: "INPUT_CHECK_RUN_ID",
-        payloadValue: payload?.checkRunId,
-        backup: inputs["check-run-id"],
+        payloadValue: payload?.checkRunId ?? "",
+        backup: inputs["check-run-id"] ?? "",
       },
-      { varname: "INPUT_DEBUG", payloadValue: payload?.debug, backup: inputs.debug },
+      { varname: "INPUT_DEBUG", payloadValue: payload?.debug ?? "", backup: inputs.debug ?? "" },
       {
         varname: "INPUT_GITHUB_REF_NAME",
-        payloadValue: payload?.targetRefName,
-        backup: process.env.GITHUB_REF_NAME,
+        payloadValue: payload?.targetRefName ?? "",
+        backup: process.env.GITHUB_REF_NAME ?? "",
       },
-      { varname: "INPUT_LABEL", payloadValue: payload?.label, backup: inputs.label },
+      { varname: "INPUT_LABEL", payloadValue: payload?.label ?? "", backup: inputs.label ?? "" },
       {
         varname: "INPUT_SETUP_CACHE_KEY",
-        payloadValue: payload?.setupCacheKey,
-        backup: inputs["cache-key"],
+        payloadValue: payload?.setupCacheKey ?? "",
+        backup: inputs["cache-key"] ?? "",
       },
       {
         varname: "INPUT_SETUP_DEPS",
-        payloadValue: payload?.setupDeps,
-        backup: inputs["setup-deps"],
+        payloadValue: payload?.setupDeps ?? "",
+        backup: inputs["setup-deps"] ?? "",
       },
-      { varname: "INPUT_TARGET_CHECKOUT", payloadValue: payload?.targetCheckout, backup: "" },
+      { varname: "INPUT_TARGET_CHECKOUT", payloadValue: payload?.targetCheckout ?? "", backup: "" },
       {
         varname: "INPUT_TARGET_CHECKOUT_REF",
-        payloadValue: payload?.targetCheckoutRef,
+        payloadValue: payload?.targetCheckoutRef ?? "",
         backup: "",
       },
       {
         varname: "INPUT_TRUNK_PATH",
-        payloadValue: payload?.trunkPath,
-        backup: inputs["trunk-path"],
+        payloadValue: payload?.trunkPath ?? "",
+        backup: inputs["trunk-path"] ?? "",
       },
       {
         varname: "INPUT_UPLOAD_LANDING_STATE",
-        payloadValue: payload?.uploadLandingState,
+        payloadValue: payload?.uploadLandingState ?? "",
         backup: "false",
       },
       {
         varname: "INPUT_UPLOAD_SERIES",
-        payloadValue: payload?.uploadSeries,
-        backup: inputs["upload-series"],
+        payloadValue: payload?.uploadSeries ?? "",
+        backup: inputs["upload-series"] ?? "",
+      },
+      {
+        varname: "TRUNK_API_ADDRESS",
+        payloadValue: payload?.trunkApiAddress ?? "",
+        backup: undefined,
       },
     ];
 
-    envVarConfigs.forEach(({ varname, payloadValue, backup }) =>
-      fs.writeSync(
-        githubEnv,
-        Buffer.from(`${varname}=${(usePayload ? payloadValue : backup) ?? ""}\n`)
-      )
-    );
+    envVarConfigs.forEach(({ varname, payloadValue, backup }) => {
+      const toWrite = usePayload ? payloadValue : backup;
+      if (toWrite !== undefined) {
+        fs.writeSync(githubEnv, Buffer.from(`${varname}=${toWrite}\n`));
+      }
+    });
     fs.closeSync(githubEnv);
   } catch (error) {
     if (error instanceof Error) {
