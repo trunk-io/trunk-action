47a7d9075676208c22268baff65c3640db52980e
Maverick Chung <maverick@trunk.io>
2023-06-21 16:19:21 -0700

    ignore shellcheck and debug

--- input_setup.sh
+++ input_setup.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
-# shellcheck disable=SC2086,SC2310
+# shellcheck disable=SC2086,SC2310,SC2312
+
+# SC2310: This function is invoked in an 'if' condition so set -e will be disabled.
+#         Invoke separately if failures should cause the script to exit.
+#
+# SC2312: Consider invoking this command separately to avoid masking its return value (or use '|| true' to ignore).
 
 set -euo pipefail
 
@@ -27,10 +32,10 @@ EOF
 
 # Every inputs.field should be referenced as INPUT_FIELD later in the action. This allows
 # the field to be set either as an argument to the github action or via inputs.json.
-if [[ "$(inputs check-mode || true)" == "payload" ]]; then
+if [[ "$(inputs check-mode)" == "payload" ]]; then
 
-  INPUT_GITHUB_TOKEN=$(payload githubToken || true)
-  INPUT_TRUNK_TOKEN=$(payload trunkToken || true)
+  INPUT_GITHUB_TOKEN=$(payload githubToken)
+  INPUT_TRUNK_TOKEN=$(payload trunkToken)
   echo "::add-mask::${INPUT_GITHUB_TOKEN}"
   echo "::add-mask::${INPUT_TRUNK_TOKEN}"
 
@@ -38,35 +43,35 @@ if [[ "$(inputs check-mode || true)" == "payload" ]]; then
 INPUT_GITHUB_TOKEN=${INPUT_GITHUB_TOKEN}
 INPUT_TRUNK_TOKEN=${INPUT_TRUNK_TOKEN}
 TRUNK_TOKEN=${INPUT_TRUNK_TOKEN}
-GITHUB_EVENT_PULL_REQUEST_BASE_REPO_OWNER=$(payload pullRequest.base.repo.owner.login || true)
-GITHUB_EVENT_PULL_REQUEST_BASE_REPO_NAME=$(payload pullRequest.base.repo.name || true)
-GITHUB_EVENT_PULL_REQUEST_BASE_SHA=$(payload pullRequest.base.sha || true)
-GITHUB_EVENT_PULL_REQUEST_HEAD_REPO_FORK=$(payload pullRequest.head.repo.fork || true)
-GITHUB_EVENT_PULL_REQUEST_HEAD_SHA=$(payload pullRequest.head.sha || true)
-GITHUB_EVENT_PULL_REQUEST_NUMBER=$(payload pullRequest.number || true)
-INPUT_ARGUMENTS=$(payload arguments || true)
-INPUT_CACHE=$(payload cache || true)
-INPUT_CACHE_KEY=$(payload cacheKey || true)
-INPUT_CACHE_PATH=$(payload cachePath || true)
-INPUT_CHECK_ALL_MODE=$(payload checkAllMode || true)
-INPUT_CHECK_MODE=$(payload checkMode || true)
-INPUT_CHECK_RUN_ID=$(payload checkRunId || true)
-INPUT_DEBUG=$(payload debug || true)
-INPUT_GITHUB_REF_NAME=$(payload targetRefName || true)
-INPUT_LABEL=$(payload label || true)
-INPUT_SETUP_CACHE_KEY=$(payload setupCacheKey || true)
-INPUT_SETUP_DEPS=$(payload setupDeps || true)
-INPUT_TARGET_CHECKOUT=$(payload targetCheckout || true)
-INPUT_TARGET_CHECKOUT_REF=$(payload targetCheckoutRef || true)
-INPUT_TRUNK_PATH=$(payload trunkPath || true)
-INPUT_UPLOAD_LANDING_STATE=$(payload uploadLandingState || true)
-INPUT_UPLOAD_SERIES=$(payload uploadSeries || true)
+GITHUB_EVENT_PULL_REQUEST_BASE_REPO_OWNER=$(payload pullRequest.base.repo.owner.login)
+GITHUB_EVENT_PULL_REQUEST_BASE_REPO_NAME=$(payload pullRequest.base.repo.name)
+GITHUB_EVENT_PULL_REQUEST_BASE_SHA=$(payload pullRequest.base.sha)
+GITHUB_EVENT_PULL_REQUEST_HEAD_REPO_FORK=$(payload pullRequest.head.repo.fork)
+GITHUB_EVENT_PULL_REQUEST_HEAD_SHA=$(payload pullRequest.head.sha)
+GITHUB_EVENT_PULL_REQUEST_NUMBER=$(payload pullRequest.number)
+INPUT_ARGUMENTS=$(payload arguments)
+INPUT_CACHE=$(payload cache)
+INPUT_CACHE_KEY=$(payload cacheKey)
+INPUT_CACHE_PATH=$(payload cachePath)
+INPUT_CHECK_ALL_MODE=$(payload checkAllMode)
+INPUT_CHECK_MODE=$(payload checkMode)
+INPUT_CHECK_RUN_ID=$(payload checkRunId)
+INPUT_DEBUG=$(payload debug)
+INPUT_GITHUB_REF_NAME=$(payload targetRefName)
+INPUT_LABEL=$(payload label)
+INPUT_SETUP_CACHE_KEY=$(payload setupCacheKey)
+INPUT_SETUP_DEPS=$(payload setupDeps)
+INPUT_TARGET_CHECKOUT=$(payload targetCheckout)
+INPUT_TARGET_CHECKOUT_REF=$(payload targetCheckoutRef)
+INPUT_TRUNK_PATH=$(payload trunkPath)
+INPUT_UPLOAD_LANDING_STATE=$(payload uploadLandingState)
+INPUT_UPLOAD_SERIES=$(payload uploadSeries)
 EOF
 
 else
 
-  INPUT_GITHUB_TOKEN=$(inputs github-token || true)
-  INPUT_TRUNK_TOKEN=$(inputs trunk-token || true)
+  INPUT_GITHUB_TOKEN=$(inputs github-token)
+  INPUT_TRUNK_TOKEN=$(inputs trunk-token)
   echo "::add-mask::${INPUT_GITHUB_TOKEN}"
   echo "::add-mask::${INPUT_TRUNK_TOKEN}"
 
@@ -74,28 +79,28 @@ else
 INPUT_GITHUB_TOKEN=${INPUT_GITHUB_TOKEN}
 INPUT_TRUNK_TOKEN=${INPUT_TRUNK_TOKEN}
 TRUNK_TOKEN=${INPUT_TRUNK_TOKEN}
-GITHUB_EVENT_PULL_REQUEST_BASE_SHA=$(githubEventPR pull_request.base.sha || true)
-GITHUB_EVENT_PULL_REQUEST_HEAD_REPO_FORK=$(githubEventPR pull_request.head.repo.fork || true)
-GITHUB_EVENT_PULL_REQUEST_HEAD_SHA=$(githubEventPR pull_request.head.sha || true)
-GITHUB_EVENT_PULL_REQUEST_NUMBER=$(githubEventPR pull_request.number || true)
+GITHUB_EVENT_PULL_REQUEST_BASE_SHA=$(githubEventPR pull_request.base.sha)
+GITHUB_EVENT_PULL_REQUEST_HEAD_REPO_FORK=$(githubEventPR pull_request.head.repo.fork)
+GITHUB_EVENT_PULL_REQUEST_HEAD_SHA=$(githubEventPR pull_request.head.sha)
+GITHUB_EVENT_PULL_REQUEST_NUMBER=$(githubEventPR pull_request.number)
 GITHUB_REF_NAME=${SETUP_GITHUB_REF_NAME}
-INPUT_ARGUMENTS=$(inputs arguments || true)
-INPUT_CACHE=$(inputs cache || true)
-INPUT_CACHE_KEY=trunk-$(inputs cache-key || true)-${SETUP_RUNNER_OS}-${SETUP_FILE_HASH}
+INPUT_ARGUMENTS=$(inputs arguments)
+INPUT_CACHE=$(inputs cache)
+INPUT_CACHE_KEY=trunk-$(inputs cache-key)-${SETUP_RUNNER_OS}-${SETUP_FILE_HASH}
 INPUT_CACHE_PATH=~/.cache/trunk
-INPUT_CHECK_ALL_MODE=$(inputs check-all-mode || true)
-INPUT_CHECK_MODE=$(inputs check-mode || true)
-INPUT_CHECK_RUN_ID=$(inputs check-run-id || true)
-INPUT_DEBUG=$(inputs debug || true)
+INPUT_CHECK_ALL_MODE=$(inputs check-all-mode)
+INPUT_CHECK_MODE=$(inputs check-mode)
+INPUT_CHECK_RUN_ID=$(inputs check-run-id)
+INPUT_DEBUG=$(inputs debug)
 INPUT_GITHUB_REF_NAME=${SETUP_GITHUB_REF_NAME}
-INPUT_SETUP_DEPS=$(inputs setup-deps || true)
+INPUT_SETUP_DEPS=$(inputs setup-deps)
 INPUT_TARGET_CHECKOUT=
 INPUT_TARGET_CHECKOUT_REF=
-INPUT_LABEL=$(inputs label || true)
-INPUT_SETUP_CACHE_KEY=$(inputs cache-key || true)
-INPUT_TRUNK_PATH=$(inputs trunk-path || true)
+INPUT_LABEL=$(inputs label)
+INPUT_SETUP_CACHE_KEY=$(inputs cache-key)
+INPUT_TRUNK_PATH=$(inputs trunk-path)
 INPUT_UPLOAD_LANDING_STATE=false
-INPUT_UPLOAD_SERIES=$(inputs upload-series || true)
+INPUT_UPLOAD_SERIES=$(inputs upload-series)
 EOF
 
 fi
