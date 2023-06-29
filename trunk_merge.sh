6a9867761408f1d2d646d122ca9d916d14bae571
Sam Lijin <sam@trunk.io>
2023-06-29 15:06:49 -0700

    more diff revert

--- trunk_merge.sh
+++ trunk_merge.sh
@@ -21,8 +21,16 @@ upstream=$(git rev-parse HEAD^1)
 git_commit=$(git rev-parse HEAD^2)
 echo "Detected merge queue commit, using HEAD^1 (${upstream}) as upstream and HEAD^2 (${git_commit}) as github commit"
 
+if [[ -n ${INPUT_CHECK_RUN_ID} ]]; then
+  annotation_argument=--trunk-annotate=${INPUT_CHECK_RUN_ID}
+else
+  annotation_argument=""
+fi
+
 "${TRUNK_PATH}" check \
   --ci \
   --upstream "${upstream}" \
   --github-commit "${git_commit}" \
+  --github-label "${INPUT_LABEL}" \
+  "${annotation_argument}" \
   ${INPUT_ARGUMENTS}
