2dec9715b104ee1b3bdc873effcd256f38cbc363
Samuel Lijin <sam@trunk.io>
2023-07-05 11:11:37 -0700

    fix: make trunk_merge work again when invoked normally (#152)
    Passing `"${annotation_argument}"` results in another empty string being
    passed into `trunk check` during `trunk_merge.sh`, which the CLI
    interprets as "attempt to lint the file with name `""`".
    
    Passing `${annotation_argument}` results in Bash not passing anything to
    `trunk check`, which results in the correct behavior.
    
    Also set up tests for `trunk_merge.sh` - it's not the greatest, but it's
    better than nothing.

---
+++ action_tests/stub.js
@@ -0,0 +1,27 @@
+#!/usr/bin/env node
+
+const fs = require("fs");
+const path = require("node:path");
+
+const getArgv = () => {
+  if (path.basename(process.argv[1]) === "stub.js") {
+    return ["trunk"].concat(process.argv.slice(2));
+  }
+
+  fs.appendFileSync(
+    process.env.TRUNK_STUB_LOGS,
+    JSON.stringify({
+      error: "Failed to sanitize argv",
+      argv: process.argv,
+    })
+  );
+  fs.appendFileSync(process.env.TRUNK_STUB_LOGS, "\n");
+  return process.argv;
+};
+
+// process.argv will look like ['/abs/path/to/node', '/abs/path/to/stub.js', ...]
+// We only want to assert that the calls look like ['trunk', 'check', '--ci', ...], hence the rewrite
+const argv = getArgv();
+
+fs.appendFileSync(process.env.TRUNK_STUB_LOGS, JSON.stringify(argv));
+fs.appendFileSync(process.env.TRUNK_STUB_LOGS, "\n");
