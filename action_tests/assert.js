d86391624d0c1742391d29ffcddb56b01adc48aa
Sam Lijin <sam@trunk.io>
2023-07-12 13:48:40 -0700

    fix

--- action_tests/assert.js
+++ action_tests/assert.js
@@ -11,10 +11,28 @@ const stubLog = fs.readFileSync(process.env.TRUNK_STUB_LOGS, "utf8").split("\n")
 // The last element of the split() should be an empty string
 expect(stubLog.slice(-1)).to.deep.equal([""]);
 
-const mktempReturned = path.join(process.env.TMPDIR, fs.readdirSync(process.env.TMPDIR)[0]);
+const getHtlFactoriesPath = () => {
+  const tmpdirContents = fs.readdirSync(process.env.TMPDIR);
 
-const EXPECTED_CLI_CALLS_BY_TEST_CASE = {
-  "trunk-merge": [
+  if (tmpdirContents.length === 0) {
+    throw new Error(
+      `TMPDIR=${process.env.TMPDIR} was empty; could not infer what --htl-factories-path should have been`
+    );
+  }
+
+  if (tmpdirContents.length > 1) {
+    throw new Error(
+      `TMPDIR=${process.env.TMPDIR} had multiple entries (${JSON.stringify(
+        tmpdirContents
+      )}); could not infer what --htl-factories-path should have been`
+    );
+  }
+
+  return path.join(process.env.TMPDIR, tmpdirContents[0]);
+};
+
+const EXPECTED_CLI_CALL_FACTORIES = {
+  "trunk-merge": () => [
     [
       "trunk",
       "check",
@@ -27,25 +45,25 @@ const EXPECTED_CLI_CALLS_BY_TEST_CASE = {
       "",
     ],
   ],
-  "all-hold-the-line-new-series": [
-    ["trunk", "check", "get-latest-raw-output", "--series", "series-name", mktempReturned],
+  "all-hold-the-line-new-series": () => [
+    ["trunk", "check", "get-latest-raw-output", "--series", "series-name", getHtlFactoriesPath()],
     ["trunk", "check", "--all", "--upload", "--series", "series-name"],
   ],
-  "all-hold-the-line-existing-series": [
-    ["trunk", "check", "get-latest-raw-output", "--series", "series-name"],
+  "all-hold-the-line-existing-series": () => [
+    ["trunk", "check", "get-latest-raw-output", "--series", "series-name", getHtlFactoriesPath()],
     [
       "trunk",
       "check",
       "--all",
       "--upload",
-      `--htl-factories-path=${mktempReturned}`,
+      `--htl-factories-path=${getHtlFactoriesPath()}`,
       "--series",
       "series-name",
     ],
   ],
 };
 
-const expectedCliCalls = EXPECTED_CLI_CALLS_BY_TEST_CASE[process.argv[2]];
+const expectedCliCalls = EXPECTED_CLI_CALL_FACTORIES[process.argv[2]]();
 
 // Strip the last element before JSON.parse, because '' is not valid JSON.
 expect(stubLog.slice(0, -1).map(JSON.parse)).to.deep.equal(expectedCliCalls);
