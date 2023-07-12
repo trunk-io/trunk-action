df22eebed5c549f313b8d4b0e67df0d888c5c284
Sam Lijin <sam@trunk.io>
2023-07-12 13:18:10 -0700

    fix: use correct regex for new series in all.sh

--- action_tests/assert.js
+++ action_tests/assert.js
@@ -10,17 +10,29 @@ const stubLog = fs.readFileSync(process.env.TRUNK_STUB_LOGS, "utf8").split("\n")
 // The last element of the split() should be an empty string
 expect(stubLog.slice(-1)).to.deep.equal([""]);
 
-// Strip the last element before JSON.parse, because '' is not valid JSON.
-expect(stubLog.slice(0, -1).map(JSON.parse)).to.deep.equal([
-  [
-    "trunk",
-    "check",
-    "--ci",
-    "--upstream",
-    process.env.EXPECTED_UPSTREAM,
-    "--github-commit",
-    process.env.EXPECTED_GITHUB_COMMIT,
-    "--github-label",
-    "",
+const EXPECTED_OUTPUTS = {
+  "trunk-merge": [
+    [
+      "trunk",
+      "check",
+      "--ci",
+      "--upstream",
+      process.env.EXPECTED_UPSTREAM,
+      "--github-commit",
+      process.env.EXPECTED_GITHUB_COMMIT,
+      "--github-label",
+      "",
+    ],
   ],
-]);
+  "all-hold-the-line-new-series": [
+    ["trunk", "check", "get-latest-raw-output", "--series", "series-name"],
+    ["trunk", "check", "--all", "--upload", "--series", "series-name"],
+  ],
+  "all-hold-the-line-existing-series": [
+    ["trunk", "check", "get-latest-raw-output", "--series", "series-name"],
+    ["trunk", "check", "--all", "--upload", "--series", "series-name"],
+  ],
+};
+
+// Strip the last element before JSON.parse, because '' is not valid JSON.
+expect(stubLog.slice(0, -1).map(JSON.parse)).to.deep.equal(EXPECTED_OUTPUTS.get(process.argv[2]));
---
+++ action_tests/setup.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+set -euo pipefail
+
+src_repo=$1
+output_repo=$2
+
+## Set up repo in `src_repo`
+
+mkdir -p "${src_repo}"
+(
+  cd "${src_repo}"
+
+  git init
+
+  git config user.name 'trunk-io/trunk-action action_tests.trunk_merge'
+  git config user.email 'action_tests@trunk-action.trunk.io'
+
+  echo "This is a README." >>readme.md
+  git add .
+  git commit --all --message 'First commit'
+
+  echo "These are contribution guidelines." >>contributing.md
+  git add .
+  git commit --all --message 'Second commit'
+
+  ## Prepare a branch `feature-branch`
+
+  git checkout -b feature-branch main^
+
+  echo "markdown file for merge test" >>merge-test.md
+  git add .
+  git commit --all --message 'add file for merge test setup'
+
+  ## Prepare a merge graph commit
+
+  git checkout -b trunk-merge/of-feature-branch main
+
+  git merge --no-ff --no-edit feature-branch
+
+  ## Go back to `main`
+
+  git checkout main
+
+  ## Dump the state of the repo
+
+  git log --graph --pretty=oneline trunk-merge/of-feature-branch
+)
+
+## Set up repo in `output_repo`
+
+git clone "${src_repo}" "${output_repo}"
