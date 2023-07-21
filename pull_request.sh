4844311eafda6267a3c3ef7ff424816e71e237be
Jenny Ma <jenny@trunk.io>
2023-07-21 12:53:59 -0400

    backtrack fix?

--- pull_request.sh
+++ pull_request.sh
@@ -52,12 +52,12 @@ if [[ -n ${INPUT_AUTOFIX_AND_PUSH} ]]; then
   "${TRUNK_PATH}" check --ci --upstream "${upstream}" --fix
   git config --global user.email ""
   git config --global user.name ""
-  git diff-index --quiet HEAD --
-  if git diff-index --quiet HEAD --; then
-    git commit --allow-empty -m "No autofixes needed!"
-  else
-    git commit -a -m "Trunk check applied autofixes automatically"
-  fi
+  # git diff-index --quiet HEAD --
+  # if git diff-index --quiet HEAD --; then
+  #   git commit --allow-empty -m "No autofixes needed!"
+  # else
+  git commit -a -m "Trunk check applied autofixes automatically"
+  # fi
   git push origin "${INPUT_GITHUB_REF_NAME}"
 else
   "${TRUNK_PATH}" check \
