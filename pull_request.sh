599a7c06c188ba0684b63f2c54951836c3e6b491
Jenny Ma <jenny@trunk.io>
2023-07-21 14:05:37 -0400

    what is happening

--- pull_request.sh
+++ pull_request.sh
@@ -27,7 +27,9 @@ fi
 
 if [[ -z ${upstream+x} ]]; then
   # Otherwise use github.event.pull_request.base.sha as the upstream.
+  echo "hi, i am in here"
   upstream="${GITHUB_EVENT_PULL_REQUEST_BASE_SHA}"
+  echo upstream
   git_commit="${GITHUB_EVENT_PULL_REQUEST_HEAD_SHA}"
   fetch origin "${upstream}"
 fi
