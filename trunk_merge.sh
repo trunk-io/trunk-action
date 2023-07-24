6133c40b7824b47db43a9bef4e2535b08fb262b1
Jenny Ma <jenny@trunk.io>
2023-07-24 16:31:41 -0400

    modify scripts to account for tests

--- trunk_merge.sh
+++ trunk_merge.sh
@@ -15,11 +15,16 @@ fetch() {
     "$@"
 }
 
-head_sha=$(git rev-parse HEAD)
-fetch --depth=2 origin "${head_sha}"
-upstream=$(git rev-parse HEAD^1)
-git_commit=$(git rev-parse HEAD^2)
-echo "Detected merge queue commit, using HEAD^1 (${upstream}) as upstream and HEAD^2 (${git_commit}) as github commit"
+if [[ -z ${TEST_GITHUB_EVENT_PATH+x} ]]; then
+  head_sha=$(git rev-parse HEAD)
+  fetch --depth=2 origin "${head_sha}"
+  upstream=$(git rev-parse HEAD^1)
+  git_commit=$(git rev-parse HEAD^2)
+  echo "Detected merge queue commit, using HEAD^1 (${upstream}) as upstream and HEAD^2 (${git_commit}) as github commit"
+else
+  upstream="${EXPECTED_UPSTREAM}"
+  git_commit="${EXPECTED_GITHUB_COMMIT}"
+fi
 
 if [[ -n ${INPUT_CHECK_RUN_ID} ]]; then
   annotation_argument=--trunk-annotate=${INPUT_CHECK_RUN_ID}
