e8b8ade44355172c70b4d6e95da560d0dd474c9e
Jenny Ma <jenny@trunk.io>
2023-07-24 18:04:14 -0400

    switch to trunk token oops

--- action_tests/assert.js
+++ action_tests/assert.js
@@ -119,7 +119,7 @@ const EXPECTED_CLI_CALL_FACTORIES = {
       "--series",
       "main",
       "--token",
-      process.env.GITHUB_TOKEN,
+      process.env.INPUT_TRUNK_TOKEN,
       '"',
       '--replace"',
       ":",
